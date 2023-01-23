18138f8a92c07739d54147878092eae49ea639c3
Mixxxxx358 <j.h.hoestra@student.tue.nl>
2023-01-23 14:47:37 +0100

    add systems

--- NonlinearController/controllers.py
+++ NonlinearController/controllers.py
@@ -1,6 +1,6 @@
 import numpy as np
 from NonlinearController.mpc_utils import *
-from systems import LTI
+from NonlinearController.systems import LTI
 from matplotlib import pyplot as plt
 from casadi import *
 from NonlinearController.model_utils import *
