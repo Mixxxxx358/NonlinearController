2ad841d427db95ac9663fde457ec15ecd5e003d9
Mixxx358 <janhh9@hotmail.com>
2023-07-11 17:31:02 +0200

    add autograd to controllers

--- NonlinearController/controllers.py
+++ NonlinearController/controllers.py
@@ -55,7 +55,7 @@ class VelocityMpcController():
         self.ne = 1
 
         self.model = model
-        if model.expr_rk4 is not None:
+        if type(self.model) == CasADi_model:
             self.model_type = "CasADi"
         else:
             self.model_type = "encoder"
@@ -93,10 +93,16 @@ class VelocityMpcController():
         self.Ge = np.zeros((self.Nc*self.nu+self.ne,self.Nc*self.nu+self.ne)) 
         self.Ge[-self.ne:,-self.ne:] = e_lambda
 
-        self.embedder = CasADi_velocity_lpv_embedder(model=self.model, Nc=self.Nc, n_stages=self.n_stages, numerical_method=self.numerical_method)
+        if self.model_type == "encoder":
+            self.embedder = velocity_lpv_embedder_autograd(ss_enc=self.model, Nc=self.Nc, n_stages=self.n_stages)
+        else:
+            self.embedder = CasADi_velocity_lpv_embedder(model=self.model, Nc=self.Nc, n_stages=self.n_stages, numerical_method=self.numerical_method)
 
         # normalize initial input and output
-        self.norm = normalizer(np.array([0.0]), np.array([1.0]), 0, 1)#model.norm
+        if self.model_type == "encoder":
+            self.norm = self.model.norm
+        else:
+            self.norm = normalizer(np.array([0.0]), np.array([1.0]), 0, 1)
         self.u0 = norm_input(self.w0, self.norm)
         self.y0 = norm_output(self.q0, self.norm)
 
@@ -115,7 +121,14 @@ class VelocityMpcController():
         self.Sx = np.hstack((np.zeros((self.nx,(self.Nc-1)*self.nz+self.ny)),np.eye(self.nx)))
 
         # initial predicted states, input, and output
-        self.X_1 = np.tile(np.zeros((1,2)),self.Nc+2).T # This cannot be assumed always. Depends on initial conditions of system
+        if self.model_type == "encoder":
+            self.nb = self.model.nb
+            self.uhist = torch.ones((1,self.nb))*self.u0
+            self.na = self.model.na
+            self.yhist = torch.Tensor(self.y0[np.newaxis].T).repeat(1,self.na+1)[None,:,:]
+            self.X_1 = np.tile(self.model.encoder(self.uhist,self.yhist).detach().numpy(),self.Nc+2).T
+        else:
+            self.X_1 = np.tile(np.zeros((1,2)),self.Nc+2).T # This cannot be assumed always. Depends on initial conditions of system
         self.U_1 = np.ones((self.Nc+1)*self.nu)[np.newaxis].T*self.u0
         self.Y_1 = np.tile(self.y0[np.newaxis],self.Nc).T
 
@@ -144,6 +157,7 @@ class VelocityMpcController():
         """
 
         reference = self.convertReference(reference)
+        reference = norm_output(reference, self.norm)
         r = extendReference(reference, 0, self.ny, self.Nc)
     
         #++++++++++++++++++ start iteration +++++++++++++++++++++++
@@ -166,7 +180,7 @@ class VelocityMpcController():
             G = 2*(Gamma.T @ (Z.T @ (self.Omega1 + self.Sy.T @ self.P @ self.Sy) @ Z + self.Omega2) @ Gamma + self.Psi)
             F = 2*(Gamma.T @ (Z.T @ (self.Omega1 @ (Z @ Phi @ Z0[:self.nz] - r) + \
                                      self.Sy.T @ self.P @ (self.Sy @ Z @ Phi @ Z0[:self.nz] - r[-self.ny:])) + self.Omega2 @ Phi @ Z0[:self.nz]))
-            
+
             # describe inequality constraints
             L = (self.M @ Gamma + self.E @ self.Lambda)
             alpha = np.ones((self.Nc,1))*self.U_1[0,0]
@@ -195,18 +209,23 @@ class VelocityMpcController():
                 self.U_1[(i*self.nu):(i*self.nu+self.nu),:] = dU0[((i-1)*self.nu):((i-1)*self.nu+self.nu),:].copy() \
                     + self.U_1[((i-1)*self.nu):((i-1)*self.nu+self.nu),:].copy()
 
-            # simuate X1 with RK4 from U0 computed above
-            x_sim = self.X_1[self.nx:self.nx*2,0].copy()
-            U_sim = self.U_1[self.nu:,0].copy()
-            X1 = np.zeros((self.nx*self.Nc,1))
-            for j in range(self.Nc):
-                x_sim = self.system.f(x_sim, U_sim[j])
-                X1[(j)*self.nx:(j+1)*self.nx,0] = x_sim.copy()
+            # # simuate X1 with RK4 from U0 computed above
+            # x_sim = self.X_1[self.nx:self.nx*2,0].copy()
+            # U_sim = self.U_1[self.nu:,0].copy()
+            # X1 = np.zeros((self.nx*self.Nc,1))
+            # for j in range(self.Nc):
+            #     x_sim = self.system.f(x_sim, U_sim[j])
+            #     X1[(j)*self.nx:(j+1)*self.nx,0] = x_sim.copy()
 
-            # Determine Y0 and dX1 from X1 and previous data
-            dX1 = X1 - np.hstack((self.X_1[self.nx:self.nx*2,0],X1[:-self.nx,0]))[np.newaxis].T
-            Y1 = np.hstack(np.split(X1,self.Nc))[1,:][np.newaxis].T
-            Y0 = np.vstack((self.Y_1[self.ny:self.ny*2,:], Y1[:-self.ny,:]))
+            # # Determine Y0 and dX1 from X1 and previous data
+            # dX1 = X1 - np.hstack((self.X_1[self.nx:self.nx*2,0],X1[:-self.nx,0]))[np.newaxis].T
+            # Y1 = np.hstack(np.split(X1,self.Nc))[1,:][np.newaxis].T
+            # Y0 = np.vstack((self.Y_1[self.ny:self.ny*2,:], Y1[:-self.ny,:]))
+
+            # predict states
+            Z1 = Phi @ Z0[:self.nz] + Gamma @ dU0
+            # split extended state up into ouputs and velocity states
+            Y0, dX1 = decodeState(Z1, self.nx, self.ny, self.Nc)
 
             # overwrite previous predicted states and output with new predicted states and output
             self.Y_1[2*self.ny:,0] = Y0[self.ny:-self.ny,0].copy(); dX0[self.nx:,0] = dX1[:-self.nx,0].copy() #change the shifting on the output to be consequential
@@ -218,7 +237,7 @@ class VelocityMpcController():
             self.X_1[-self.nx:,:] = dX1[-self.nx:,:] + self.X_1[-2*self.nx:-self.nx,:]
 
             # stopping condition
-            if np.linalg.norm(self.U_1 - U_1_old) < 1e-2:
+            if np.linalg.norm(self.U_1 - U_1_old) < 1e-1:
                 break
 
         u0 = self.U_1[self.nu:self.nu*2,0].copy()
@@ -265,6 +284,17 @@ class VelocityMpcController():
         """
         y1 = norm_output(q1, self.norm)
         u0 = norm_input(w0, self.norm)
+
+        if self.model_type == "encoder":
+            # shift history input and output for encoder
+            for j in range(self.nb-1):
+                self.uhist[0,j] = self.uhist[0,j+1]
+            self.uhist[0,self.nb-1] = torch.Tensor(u0)
+            for j in range(self.na):
+                self.yhist[0,:,j] = self.yhist[0,:,j+1]
+            self.yhist[0,:,self.na] = torch.Tensor([y1])
+            # predict state with encoder
+            x1 = self.model.encoder(self.uhist,self.yhist).detach().numpy().T
 
         self.X_1[:-self.nx, :] = self.X_1[self.nx:, :]; self.X_1[self.nx:2*self.nx, :] = x1.copy()
         self.U_1[:-self.nu, :] = self.U_1[self.nu:, :]
