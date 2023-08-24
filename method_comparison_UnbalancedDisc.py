18ef8c704edb702ec373642a2be8465ec184ec22
Mixxxxx358 <j.h.hoestra@student.tue.nl>
2023-08-24 11:31:18 +0200

    add correct plotting

--- method_comparison_UnbalancedDisc.py
+++ method_comparison_UnbalancedDisc.py
@@ -15,33 +15,33 @@ nu = system.nu if system.nu is not None else 1
 ny = system.ny if system.ny is not None else 1
 
 ##################  Model  #######################
-# ode = odeCasADiUnbalancedDisc()
-# model = CasADi_model(ode, (1), dt, nx=2, nu=nu)
-model = deepSI.load_system("NonlinearController/trained_models/unbalanced/ud_test_4")
+ode = odeCasADiUnbalancedDisc()
+model = CasADi_model(ode, (1), dt, nx=2, nu=nu)
+# model = deepSI.load_system("NonlinearController/trained_models/unbalanced/ud_test_4")
 
 ##################  MPC controller parameters  #######################
-Nc=10; max_iter = 1; nr_sim_steps = 100
+Nc=5; max_iter = 1; nr_sim_steps = 100
 wlim = 5
 qlim = 1.0
 nx = 2; nz = nx+ny; ne = 1
 
-Q1 = np.zeros((ny,ny)); np.fill_diagonal(Q1, [100])
+Q1 = np.zeros((ny,ny)); np.fill_diagonal(Q1, [1e2])
 Q2 = np.zeros((nz,nz)); Q2[ny:,ny:] = np.eye(nx)*1
-R = np.eye(nu)*0.1
+R = np.eye(nu)*0.01
 P = np.eye(ny)*0
 
 ##################  Reference  #######################
-# a = 3.1; reference_theta = np.hstack((np.ones(20)*a,np.ones(20)*-a,np.ones(20)*a/2,np.ones(20)*-a/2,np.ones(40)*0))
+a = 3.1; reference_theta = np.hstack((np.ones(20)*a,np.ones(20)*-a,np.ones(20)*a/2,np.ones(20)*-a/2,np.ones(40)*0))
 # reference_theta = np.load("NonlinearController/references/multisine.npy")
 
 # reference_theta = randomLevelReference(nr_sim_steps+Nc,[10,15],[-3.1,3.1])
-reference_theta = np.load("NonlinearController/references/setPoints.npy")
-# reference_theta = deepSI.deepSI.exp_design.multisine(nr_sim_steps+Nc, pmax=20, n_crest_factor_optim=10)*1.5
+# reference_theta = np.load("NonlinearController/references/setPoints.npy")
+# reference_theta = deepSI.deepSI.exp_design.multisine(nr_sim_steps+Nc, pmax=17, n_crest_factor_optim=10)*1.7
 # reference_theta = np.sin(np.arange(0,nr_sim_steps+Nc)/np.pi*2.0)*3.1
 reference = reference_theta[np.newaxis]
 
 ##################  Control Loop MVT  #######################
-steps = np.arange(1,10,1)
+steps = np.arange(1,15,1)
 log_w_inf, log_q_inf = controlLoop(reference_theta, system, model, Nc, nr_sim_steps, nu, ny, Q1, Q2, R, P, qlim, wlim, max_iter, 200, 3, "LPV")
 inf_diff = (log_q_inf[0,:] - reference[0,:nr_sim_steps]); DR_inf = inf_diff.T @ inf_diff
 
@@ -81,9 +81,14 @@ for i in range(len(steps)):
 ##################  Plots  #######################
 fig1 = plt.figure(figsize=[8.9, 8])
 plt.plot(diff_MMVT, label="MMVT")
-plt.plot(diff_Mid, label="Rect")
-plt.plot(diff_Trap, label="Trap")
+plt.plot(diff_Mid, label="Midpoint")
+plt.plot(diff_Trap, label="Trapezian")
 plt.plot(diff_Simps, label="Simpsons")
+plt.xlabel("integration steps")
+plt.ylabel("RCSO")
 plt.legend()
 plt.grid()
+
+plt.savefig("Figures/NumericalConvergenceLevels.svg")
+
 plt.show()
